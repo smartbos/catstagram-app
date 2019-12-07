eb875dc3b5af1722dabb9dcc7ccf0f153ca3a404
Hyunseok Lee <smartbosslee@gmail.com>
2019-12-08 00:23:43 +0900

    리스트로 표시해 봄

--- lib/main.dart
+++ lib/main.dart
@@ -43,18 +43,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final items = [
+    'https://pbs.twimg.com/media/ByA3wtYIIAAOwaK.jpg',
+    'https://pbs.twimg.com/media/ByA3wtYIIAAOwaK.jpg',
+    'https://pbs.twimg.com/media/ByA3wtYIIAAOwaK.jpg',
+    'https://pbs.twimg.com/media/ByA3wtYIIAAOwaK.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -71,31 +65,26 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Catstagram', style: TextStyle(color: Colors.black87)),
         backgroundColor: Colors.white,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CachedNetworkImage(
-              imageUrl: 'https://pbs.twimg.com/media/ByA3wtYIIAAOwaK.jpg',
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CachedNetworkImage(
+                  imageUrl: '${items[index]}',
+                ),
+                Text('text')
+              ],
             ),
-          ],
-        ),
+          );
+//          return ListTile(
+//            title: CachedNetworkImage(
+//              imageUrl: '${items[index]}',
+//            ),
+//          );
+        },
       ),
     );
   }
